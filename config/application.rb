1fae1623c8a31340d8722daf5ac60b8fd0fe9ed9
Tristan Hart <tristanhart@gmail.com>
2018-04-10 20:25:13 +1000

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SlickGiftsBackend
+  class Application < Rails::Application
+    # Autoload all the files in the lib directory
+    # so that we don't need to require them every time
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
+
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    # Default primary keys to UUID
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
+    # Use SQL for the schema, so we can do things like index on LOWER(name)
+    config.active_record.schema_format = :sql
+
+    # Use sidekiq for async jobs
+    config.active_job.queue_adapter = :sidekiq
+
+    # Allow requests from other origins
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+                 headers: :any,
+                 methods: %i[get post delete put patch options head]
+      end
+    end
+  end
+end
