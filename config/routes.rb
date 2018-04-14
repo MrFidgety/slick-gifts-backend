f8254a908fefa0650441f19861467519c7700378
Tristan Hart <tristanhart@gmail.com>
2018-04-14 21:57:39 +1000

    Add route and request spec for creating a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_scope :user do
+        resources :users, only: %i[create]
+      end
+
+      namespace :users do
+        resource :me, only: %i[show], controller: :my_users
+      end
+    end
+  end
 end
