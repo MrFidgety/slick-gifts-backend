4eb980ea85d90162c8f9cdf97f210d9419c4ea8a
Tristan Hart <tristanhart@gmail.com>
2018-04-18 23:09:27 +1000

    Set default url host for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Sets a default url for things like action_mailer
+  default_url_options[:host] = "http://localhost:5000"
 end
