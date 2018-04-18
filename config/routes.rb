e98fea08e3f40ab95543cbeb83d03f2a0cdc5dcf
Tristan Hart <tristanhart@gmail.com>
2018-04-18 23:47:34 +1000

    Update route for devise confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      devise_for :users, skip: :all
-
-      resources :users, only: %i[create]
-
-      namespace :user do
-        # confirmation url required for devise confirmable
-        resource :confirmation, only: %i[show]
-      end
-
-      namespace :users do
-        resource :me, only: %i[show], controller: :my_users
+      resources :users, only: %i[create] do
+        collection do
+          # Set confirmation url used by devise
+          resource :confirmation, only: %i[show],
+                                  as: :user_confirmation
+        end
       end
     end
   end
