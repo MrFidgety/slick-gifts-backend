f183182a7bb9fa1f87bc37cf1002f8c00f68a5b7
Tristan Hart <tristanhart@gmail.com>
2018-04-25 21:57:11 +1000

    Add serializer for sessions

---
+++ spec/serializers/session_serializer_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe SessionSerializer, type: :serializer do
+  include_context "serializer"
+
+  let(:resource) { build(:session) }
+
+  describe "attributes" do
+    let(:attributes) do
+      %w[id authentication_token]
+    end
+
+    it { is_expected.to serialize_attributes(attributes) }
+  end
+
+  describe "associations" do
+    it do
+      user = create(:user, sessions: [resource])
+      is_expected.to serialize_has_one(user).with_key("authable")
+    end
+  end
+end
