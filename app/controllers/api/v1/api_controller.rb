76af3afe1cd1c161e45ee2d6cc1cc636f7099671
Tristan Hart <tristanhart@gmail.com>
2018-05-01 21:43:41 +1000

    Move current_user method to session auth concern

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -8,18 +8,10 @@ module Api
 
       renderror_auto_rescue :bad_request, :invalid_document
 
-      skip_before_action :verify_authenticity_token
-
       include Api::SessionAuthentication
       include Api::ValidateRequestBody
       include Api::ResponseOptions
       include Api::TopLevelMeta
-
-      private
-
-        def current_authable
-          current_user
-        end
     end
   end
 end
